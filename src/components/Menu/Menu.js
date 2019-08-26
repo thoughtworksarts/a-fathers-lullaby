589f70bfc6ba7f35d3b545e90ecae4936f2d1cf9
austinratcliff <hello@austinratcliff.com>
2019-08-26 14:40:57 -0500

    [#15][Austin/Brennan] Remove Font Awesome menu icon to use SVG file

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../../assets/logo-small.png';
+import menuBars from '../../assets/menu-bars.svg';
 import './Menu.css';
 
 const Menu = () => {
@@ -14,10 +15,11 @@ const Menu = () => {
 
   return (
     <nav className="Menu">
-      <a href="/"><img src={logo} alt="A Father's Lullaby" /></a>
+      <a href="/"><img className="logo" src={logo} alt="A Father's Lullaby" /></a>
       {
         isMobile ?
-          <i className="fas fa-bars"></i>
+          // <i className="fas fa-bars"></i>
+          <img className="menu-bars" src={menuBars} alt="Menu" />
         :
           <ul>
             <li><a href="/" className="selected">About</a></li>
