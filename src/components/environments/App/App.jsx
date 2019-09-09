3c9a6b8f963886234d1e64b2782e03b7389290d8
Tim Brandle <tbrandle53@gmail.com>
2019-09-09 10:14:44 -0600

    moved Router to index.js

--- src/components/environments/App/App.jsx
+++ src/components/environments/App/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { Header } from 'molecules'
 import { About, Share, Listen } from 'pages'
 import './App.css'
 
 function App () {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Switch>
-          <Route path='/' exact component={About} />
-          <Route path='/share' component={Share} />
-          <Route path='/listen' component={Listen} />
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      <Header />
+      <Switch>
+        <Route path='/' exact component={About} />
+        <Route path='/share' component={Share} />
+        <Route path='/listen' component={Listen} />
+      </Switch>
+    </div>
   )
 }
 
