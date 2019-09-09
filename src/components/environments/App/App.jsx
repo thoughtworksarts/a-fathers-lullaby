788b4a089b4ec887132897b2bed6cd3fb34a5659
Tim Brandle <tbrandle53@gmail.com>
2019-09-09 11:05:39 -0600

    [#40][Tim] Add pre-commit hook for linting

--- src/components/environments/App/App.jsx
+++ src/components/environments/App/App.jsx
@@ -9,7 +9,7 @@ function App () {
     <div className='App'>
       <Header />
       <Switch>
-        <Route path='/' exact component={About} />
+        <Route exact path='/' component={About} />
         <Route path='/share' component={Share} />
         <Route path='/listen' component={Listen} />
       </Switch>
