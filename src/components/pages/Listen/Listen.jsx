cfcab890c008ecc2b7694ed9b913a7435cf5a728
Marisa Hoenig <marisa.hoenig@gmail.com>
2019-09-09 11:56:54 -0400

    [#39][Marisa] Router linting changes

--- src/components/pages/Listen/Listen.jsx
+++ src/components/pages/Listen/Listen.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Listen.css';
+import React from 'react'
+import './Listen.css'
 
 const Listen = () => {
   return (
-    <div className="ListenPage">
+    <div className='ListenPage'>
       <h1>Listen</h1>
     </div>
-  );
+  )
 }
 
-export default Listen;
+export default Listen
