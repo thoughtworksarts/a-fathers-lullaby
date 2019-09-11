2b3dc0fb3ba77066beb3455427f0736992ffcf2a
Tim Brandle <tbrandle53@gmail.com>
2019-09-11 14:48:46 -0600

    fix linting

--- cypress/integration/about/visit_about_page.spec.js
+++ cypress/integration/about/visit_about_page.spec.js
@@ -1,6 +1,6 @@
 context('Visit about page', () => {
   it('should show about text', () => {
     cy.visit('http://localhost:3000')
-    cy.get('.AboutPage').should('contain', 'About');
+    cy.get('.AboutPage').should('contain', 'About')
   })
 })
