240eecb8e1116a4d478d348be1563bb63842f7b9
austinratcliff <hello@austinratcliff.com>
2019-09-17 16:06:25 -0500

    [#37][Austin/Stephanie/Brennan] Modified public URL in build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -2,13 +2,13 @@ FILE="./.env"
 
 if [[ $TRAVIS_BRANCH == 'release' ]]; then
 cat <<EOM >$FILE
-PUBLIC_URL=https://thoughtworksarts.io/a-fathers-lullaby/
+PUBLIC_URL=/a-fathers-lullaby
 EOM
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
 elif [[ $TRAVIS_BRANCH == 'master' ]]; then
 cat <<EOM >$FILE
-PUBLIC_URL=https://a-fathers-lullaby.surge.sh/
+PUBLIC_URL=/
 EOM
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
