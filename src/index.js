d542d16995cb068edecef11d41aa6e7c636d6213
Brennan Pilcher <brennan.pilcher@thoughtworks.com>
2019-09-17 16:31:46 -0400

    [#37][Austin/Stephanie/Brennan] Explicitly set public_url environment variable before building.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 import { App } from 'environments'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Router basename={process.env.REACT_APP_BASENAME}><App /></Router>, document.getElementById('root'))
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
