0825193f51f447808158ed35374b808183c90d37
Tim Brandle <tbrandle53@gmail.com>
2019-09-16 19:16:05 -0600

    linting

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { withTracking } from 'helpers'
 
 export const Button = ({ children, onClickWithTracking, onClick, gaTrack = true }) => {
-  console.log('onClickWithTracking', onClickWithTracking);
-  
+  console.log('onClickWithTracking', onClickWithTracking)
+
   return (
     <button onClick={gaTrack ? onClickWithTracking('Button') : onClick}>
       {children}
