3763b0878bd22b7593056bb6592d52a3d7b29fe3
Brennan Pilcher <brennan.pilcher@thoughtworks.com>
2019-09-17 16:13:36 -0400

    [#37][Austin/Stephanie/Brennan] Attempt to set the basename to the fully qualified URL.

--- build-env.sh
+++ build-env.sh
@@ -2,13 +2,13 @@ FILE="./.env"
 
 if [[ $TRAVIS_BRANCH == 'release' ]]; then
 cat <<EOM >$FILE
-REACT_APP_BASENAME=/a-fathers-lullaby
+REACT_APP_BASENAME=https://thoughtworksarts.io/a-fathers-lullaby/
 EOM
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
 elif [[ $TRAVIS_BRANCH == 'master' ]]; then
 cat <<EOM >$FILE
-REACT_APP_BASENAME=/
+REACT_APP_BASENAME=https://a-fathers-lullaby.surge.sh/
 EOM
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
