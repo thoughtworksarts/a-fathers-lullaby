c57ad7e417b53d2b2277e1a42214539284993dda
Tim Brandle <tbrandle53@gmail.com>
2019-09-24 10:49:31 -0600

    [Tim] remove custom mapping of env files

--- config/craco.config.js
+++ config/craco.config.js
@@ -1,24 +1,3 @@
-const fs = require('fs')
-const dotenv = require('dotenv')
-
-process.traceDeprecation = true
-
-process.env.APP_ENV = process.env.APP_ENV || 'development'
-
-const envFiles = [
-  `.env.${process.env.APP_ENV}`,
-  `.env.${process.env.APP_ENV}.local`
-]
-
-envFiles.forEach(file => {
-  if (fs.existsSync(file)) {
-    const envConfig = dotenv.parse(fs.readFileSync(file))
-
-    Object.keys(envConfig).forEach(envKey => {
-      process.env[envKey] = envConfig[envKey]
-    })
-  }
-})
 
 const jestConfig = require('./jestConfig')
 const webpackConfig = require('./webpackConfig')
