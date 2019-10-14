29f94ed7d44185dbca269f6d15ab442d723b274b
Marisa Hoenig <marisa.hoenig@gmail.com>
2019-10-14 16:16:58 -0400

    [Marisa] Added Share page text and swapped in new text colors

--- src/components/pages/Share/Share.jsx
+++ src/components/pages/Share/Share.jsx
@@ -4,7 +4,14 @@ import './Share.css'
 const Share = () => {
   return (
     <div className='SharePage'>
-      <h1>Share</h1>
+      <div className='recordingTitle'>
+      Together we create a rich audio landscape of <span className='highlight'>memories and stories</span>. Prepare for your recording and build our shared experience.
+      </div>
+
+      <div className='recordingInstruction'>
+        Please <span className='highlight'>answer the following questions</span> to help listeners discover your contribution.
+      </div>
+
     </div>
   )
 }
