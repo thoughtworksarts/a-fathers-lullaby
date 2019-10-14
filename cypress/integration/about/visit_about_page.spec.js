f523c77b67fda85be4c5fed2b4700f6dce43afbb
Megan Goodling <megan.goodling@thoughtworks.com>
2019-10-14 16:03:01 -0500

    [Testing] Fixed integration test for About page to update to new text on page.

--- cypress/integration/about/visit_about_page.spec.js
+++ cypress/integration/about/visit_about_page.spec.js
@@ -1,6 +1,7 @@
 context('Visit about page', () => {
   it('should show about text', () => {
     cy.visit('http://localhost:3000')
-    cy.get('.AboutPage').should('contain', 'About')
+    cy.get('.AboutPage').should('contain', 'A Father’s Lullaby highlights the role of men in raising children while underlining the absence of fathers due to racial disparities of mass incarceration. Policies have taken their toll on children, our next generation. This contributory project invites you to record your voice, share insight, sing a lullaby and create a collective voice for this issue and to imagine social policies based on support not criminalization. The space of love and shared memories of childhood becomes the place to contemplate difficult issues our society is facing today.'
+    )
   })
 })
