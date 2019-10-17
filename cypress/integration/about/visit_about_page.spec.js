a8de09a4e6e802b7b5a57f3eb7841234f2004c9b
Marisa Hoenig <marisa.hoenig@gmail.com>
2019-10-17 16:28:25 -0500

    [#66][Marisa/Megan] Added message on About page and styling.

--- cypress/integration/about/visit_about_page.spec.js
+++ cypress/integration/about/visit_about_page.spec.js
@@ -1,7 +1,6 @@
 context('Visit about page', () => {
   it('should show about text', () => {
     cy.visit('http://localhost:3000')
-    cy.get('.AboutPage').should('contain', 'A Father’s Lullaby highlights the role of men in raising children while underlining the absence of fathers due to racial disparities of mass incarceration. Policies have taken their toll on children, our next generation. This contributory project invites you to record your voice, share insight, sing a lullaby and create a collective voice for this issue and to imagine social policies based on support not criminalization. The space of love and shared memories of childhood becomes the place to contemplate difficult issues our society is facing today.'
-    )
+    cy.get('.AboutPage').should('contain', 'Personal stories have transformative power and create profound encounters. A Father\'s Lullaby invites you to become part of a growing movement.')
   })
 })
--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './About.css'
 import { Footer } from 'molecules'
-import { Logo, Button } from 'atoms'
+import { Button } from 'atoms'
 
 const About = () => {
   return (
-    <div>
-      <div className='AboutPage'>
-        <Button to='/share'>Participate</Button>
-        <Logo size='lg' />
-        <p>A Father’s Lullaby highlights the role of men in raising children while underlining the absence of fathers due to racial disparities of mass incarceration. Policies have taken their toll on children, our next generation. This contributory project invites you to record your voice, share insight, sing a lullaby and create a collective voice for this issue and to imagine social policies based on support not criminalization. The space of love and shared memories of childhood becomes the place to contemplate difficult issues our society is facing today.</p>
-      </div>
+    <div className='AboutPage'>
+      <h1>There are <span className='highlight'>2.7 million children</span> with a parent in prison or jail. 92% of parents <span className='highlight'>in prison are fathers.</span></h1>
+      <Button to='/share'>Participate</Button>
+      <h2>Personal stories have transformative power and create profound encounters. A Father's Lullaby invites you to become part of a growing movement.</h2>
       <Footer />
     </div>
   )
