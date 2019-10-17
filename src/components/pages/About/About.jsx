50423ab51308b533ace107840820a68e8291492c
Marisa Hoenig <marisa.hoenig@gmail.com>
2019-10-17 17:00:11 -0500

    [#66][Megan/Marisa] Added About page image

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -6,9 +6,11 @@ import { Button } from 'atoms'
 const About = () => {
   return (
     <div className='AboutPage'>
-      <h1>There are <span className='highlight'>2.7 million children</span> with a parent in prison or jail. 92% of parents <span className='highlight'>in prison are fathers.</span></h1>
-      <Button to='/share'>Participate</Button>
-      <h2>Personal stories have transformative power and create profound encounters. A Father's Lullaby invites you to become part of a growing movement.</h2>
+      <div className='intro'>
+        <h1>There are <span className='highlight'>2.7 million children</span> with a parent in prison or jail. 92% of parents <span className='highlight'>in prison are fathers.</span></h1>
+        <Button to='/share'>Participate</Button>
+        <h2>Personal stories have transformative power and create profound encounters. A Father's Lullaby invites you to become part of a growing movement.</h2>
+      </div>
       <Footer />
     </div>
   )
