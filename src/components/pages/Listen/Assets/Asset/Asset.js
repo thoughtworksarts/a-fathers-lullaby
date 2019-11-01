7fba1ace7f9b2fc1e274e304b768fdc0a663130b
austinratcliff <hello@austinratcliff.com>
2019-11-01 14:07:45 -0500

    [#80][Austin/Megan] Add handler for when audio ends to continue to next story in the list unless it's the last story

--- src/components/pages/Listen/Assets/Asset/Asset.js
+++ src/components/pages/Listen/Assets/Asset/Asset.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Asset = props => {
   const minutes = Math.floor(props.asset.audio_length_in_seconds / 60)
   const seconds = Math.trunc(props.asset.audio_length_in_seconds - (minutes * 60))
+  const index = props.listNumber - 1
   const title = 'Story ' + props.asset.id
   const filename = props.asset.filename
 
   return (
-    <tr className='Asset' onClick={() => props.clickHandler(title, filename)}>
+    <tr className="Asset" onClick={() => props.clickHandler(index, title, filename)}>
       <td width='10%'>{props.listNumber}</td>
       <td width='10%'>Story {props.asset.id}</td>
       {/* TODO: Change location to reflect asset latitude and longitude with a geocoding API */}
--- src/components/pages/Listen/Assets/Assets.js
+++ src/components/pages/Listen/Assets/Assets.js
@@ -6,15 +6,17 @@ import ReactAudioPlayer from 'react-audio-player'
 import numberIcon from './hashtag-solid.svg'
 import clockIcon from './clock-regular.svg'
 
+
 const Assets = () => {
   const [assets, setAssets] = useState([])
+  const [currentStoryIndex, setCurrentStoryIndex] = useState('')
   const [currentTitle, setCurrentTitle] = useState('')
   const [currentFilename, setCurrentFilename] = useState('')
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_ASSETS_URL}`, {
+    fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_ASSETS_URL}`, {
       headers: {
-        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
+        'authorization': `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
       }
     })
       .then(res => res.json())
@@ -22,7 +24,8 @@ const Assets = () => {
       .catch(err => console.log(err))
   }, [])
 
-  const clickHandler = (title, filename) => {
+  const clickHandler = (index, title, filename) => {
+    setCurrentStoryIndex(index)
     setCurrentTitle(title)
     setCurrentFilename(filename)
   }
@@ -30,39 +33,52 @@ const Assets = () => {
   // TODO: Change to asset id if it's possible to update the current ids to start at 1
   let listNumber = 0
 
+  const endHandler = () => {
+    const nextStoryIndex = currentStoryIndex + 1
+    
+    if (nextStoryIndex === assets.length) {
+      return null
+    } else {
+      setCurrentStoryIndex(nextStoryIndex)
+      setCurrentTitle('Story ' + assets[nextStoryIndex].id)
+      setCurrentFilename(assets[nextStoryIndex].filename)
+    }
+  }
+
   return (
-    <div className='Assets'>
+    <div className="Assets">
       {
         assets.length > 0
-          ? <>
-            <div className='audio-player'>
-              <p className='current-title'>{currentTitle}</p>
-              <ReactAudioPlayer
-                src={process.env.REACT_APP_ROUNDWARE_PROD + currentFilename}
-                controls
-                autoPlay
-              />
-            </div>
-            <Table hover>
-              <thead>
-                <tr>
-                  <th><img src={numberIcon} className='number-icon' alt='number icon' /></th>
-                  <th>Title</th>
-                  <th>Location</th>
-                  <th><img src={clockIcon} className='clock-icon' alt='clock icon' /></th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  assets.map(asset => {
-                    listNumber += 1
-                    return <Asset key={asset.id} asset={asset} listNumber={listNumber} clickHandler={clickHandler} />
-                  })
-                }
-              </tbody>
-            </Table>
-          </>
-          : <h1 className='loading'>Loading...</h1>
+          ? <React.Fragment>
+              <div className="audio-player">
+                <p className="current-title">{currentTitle}</p>
+                <ReactAudioPlayer
+                  src={process.env.REACT_APP_ROUNDWARE_PROD + currentFilename}
+                  controls
+                  autoPlay
+                  onEnded={endHandler}
+                />
+              </div>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th><img src={numberIcon} className="number-icon" alt="number icon" /></th>
+                    <th>Title</th>
+                    <th>Location</th>
+                    <th><img src={clockIcon} className="clock-icon" alt="clock icon" /></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    assets.map(asset => {
+                      listNumber += 1
+                      return <Asset key={asset.id} asset={asset} listNumber={listNumber} clickHandler={clickHandler} />
+                    })
+                  }
+                </tbody>
+              </Table>
+            </React.Fragment>
+          : <h1 className="loading">Loading...</h1>
       }
     </div>
   )
