a2b26a567669258ab0e11b8b2b1e59df737c9ee2
megangoodling <megan.goodling@thoughtworks.com>
2019-11-07 15:47:30 -0600

    [Megan] Fix library page spec to fix build hopefully

--- cypress/integration/library/visit_library_page.spec.js
+++ cypress/integration/library/visit_library_page.spec.js
@@ -2,7 +2,7 @@ context('Visit library page', () => {
   it('should load all assets', () => {
     cy.visit('http://localhost:3000/library')
     cy.get('.loading').should('exist')
-    cy.get('.loading').should('not.exist', { pageLoadTimeout: 30000 })
+    cy.get('.loading', { timeout: 3000 }).should('not.exist')
     cy.get('.Asset').should('have.length.above', 0)
     cy.get('.react-audio-player').should('exist')
   })
