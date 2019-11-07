dcf8c2feb9910a51c8ad023eaa037b69234dd1e0
austinratcliff <hello@austinratcliff.com>
2019-11-07 10:35:04 -0600

    [#80][Megan/Austin] Add Router to Assets snapshot test

--- src/components/pages/Listen/Assets/Assets.test.js
+++ src/components/pages/Listen/Assets/Assets.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Assets from './Assets'
 import { shallow } from 'enzyme'
 
 describe('Assets', () => {
   it('matches snapshot', () => {
-    const assetsComponent = shallow(<Assets />)
+    const assetsComponent = shallow(<Router><Assets /></Router>)
     expect(assetsComponent).toMatchSnapshot()
   })
 })
