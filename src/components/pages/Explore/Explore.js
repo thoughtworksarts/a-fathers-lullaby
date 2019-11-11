e646fbb475cf4f49c4068017b4be5bf3e86850b4
megangoodling <megan.goodling@thoughtworks.com>
2019-11-11 12:35:05 -0600

    Removed unnecessary header and button from Explore page

--- src/components/pages/Explore/Explore.js
+++ src/components/pages/Explore/Explore.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import './Explore.css'
-
-import { Button } from 'atoms'
 import { MapContainer } from '../../organisms'
+import './Explore.css'
 
 const Explore = () => {
   return (
     <div className='ExplorePage'>
       <div className='MapView'>
-        <h1>Click on the button to see a library of all recordings.</h1>
         <MapContainer />
-        <Button to='/library'>Library</Button>
       </div>
     </div>
   )
