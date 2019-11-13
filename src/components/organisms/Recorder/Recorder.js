fae3a35a5fea563502b51287b8890de7274cb571
Matthew DeGraffenreid <matthew.d@thoughtworks.com>
2019-11-13 15:57:55 -0500

    [Stephanie/Matt][#79][Added UI Buttons for Rec,Stop,Play and Funcationality]

--- src/components/organisms/Recorder/Recorder.js
+++ src/components/organisms/Recorder/Recorder.js
@@ -6,53 +6,52 @@ import playImg from '../../../assets/play.png'
 import './Recorder.css'
 
 const Recorder = () => {
-    
   const [isRecording, setIsRecording] = useState(false)
   const [blobURL, setBlobURL] = useState('')
-  const [isBlocked/*, setIsBlocked*/] = useState(false)
+  const [isBlocked, setIsBlocked] = useState(false)
   const [buttonImg, setButtonImg] = useState(recImg)
- 
+
   useEffect(() => {
     /* This might need to get wrapped in an if statment depending on behavior */
     navigator.mediaDevices.getUserMedia({ audio: true, video: false })
       .then(function (stream) {
         /* use the stream */
         console.log('Permission Granted')
-        // Set state to is blocked to false
+        // Set state to is blocked to
       })
       .catch(function (err) {
         /* handle the error */
+        if (err) console.log(err.stack)
         console.log('Permission Denied')
         // Set state to is blocked to true
+        setIsBlocked(true)
       })
-      
-      changeImgFunc()
+
+    changeImgFunc()
   })
 
   const changeImgFunc = () => {
-      if(!isRecording && blobURL === '' ){
-        setButtonImg(recImg)
-      } 
-      else if(isRecording){
-        setButtonImg(stopImg)
-        start()
-      } 
-      else{
-        setButtonImg(playImg)
-      }
+    if (!isRecording && blobURL === '') {
+      setButtonImg(recImg)
+    } else if (isRecording) {
+      setButtonImg(stopImg)
+      start()
+    } else {
+      setButtonImg(playImg)
+    }
   }
 
   const Mp3Recorder = new MicRecorder({ bitRate: 128 })
 
   const start = () => {
     if (isBlocked) {
-      console.log('Permission Denied');
+      console.log('Permission Denied')
     } else {
       Mp3Recorder
         .start()
-        .catch((e) => console.error(e));
+        .catch((e) => console.error(e))
     }
-  };
+  }
 
   const stop = () => {
     Mp3Recorder
@@ -61,16 +60,19 @@ const Recorder = () => {
       .then(([buffer, blob]) => {
         const blobURL = URL.createObjectURL(blob)
         setBlobURL(blobURL)
-      }).catch((e) => console.log(e));
-  };
+      }).catch((e) => console.log(e))
+  }
 
   return (
-    
-    <div>  
-   
-        <img className='mainSpeakingButton' src={buttonImg} alt="Record Button" onClick={() => { 
-            if(isRecording) stop()    
-            setIsRecording(!isRecording) }}/>
+
+    <div>
+
+      <img
+        className='mainSpeakingButton' src={buttonImg} alt='Record Button' onClick={() => {
+          if (isRecording) stop()
+          setIsRecording(!isRecording)
+        }}
+      />
     </div>
 
   )
