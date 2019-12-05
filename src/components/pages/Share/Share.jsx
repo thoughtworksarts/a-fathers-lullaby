e87da07a873613864c196fa7ca7b0881dbe68416
megangoodling <megan.goodling@thoughtworks.com>
2019-12-05 15:39:40 -0600

    [#79][Megan/Matthew] Upload story roundware functionality -- POST Session

--- src/components/pages/Share/Share.jsx
+++ src/components/pages/Share/Share.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Recorder, ParticipateForm, ShareLocation } from 'organisms'
 import './Share.css'
 
@@ -10,6 +10,57 @@ const Share = () => {
     console.log('Recorded Story blob URL ' + recordedStory)
   }
 
+  useEffect(() => {
+    if (!recordedStory) return
+    /**
+     * Step 1:
+     * Connect to roundware and get a session
+     * POST localhost:8888/api/2/sessions/
+     * Validate that we got a session throw error is none is given
+     * 
+     * REACT_APP_SESSIONS_URL=https://prod.roundware.com/api/2/sessions/
+     */
+    
+    const form = '{"project_id": 1,"client_system": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}'
+    console.log(form)
+    
+    fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_SESSIONS_URL}`, {
+      method: 'POST',
+      headers: {
+        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
+        "content-type": "application/json"
+      },
+      "processData": false,
+      "body": form 
+    })
+    .then((res) => {
+      console.log(res.json())
+      })
+    .catch(err => console.log(err))
+
+      /**
+      * Step 2:
+      * Create a new envelope 
+      * POST localhost:8888/api/2/envelopes/
+      * Save the id returned
+      */
+      
+      /**
+       * Step 3:
+       * Gather all the info and create a new Formdata class
+       * 
+       */
+
+        /**
+        * Step 4:
+        * Update Envelope with Asset
+        * 
+        * PATCH localhost:8888/api/2/envelopes/:id/
+        * 
+        * Validate response
+        */
+  }, [recordedStory])
+    
   return (
 
     <div className='SharePage'>
