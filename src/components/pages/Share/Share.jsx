3d303e564114babf129dbf85b03ae155f7d969b8
megangoodling <megan.goodling@thoughtworks.com>
2019-12-06 10:44:48 -0600

    [#79][Matthew/Megan] Refactor session and envelope requests for Roundware

--- src/components/pages/Share/Share.jsx
+++ src/components/pages/Share/Share.jsx
@@ -12,55 +12,72 @@ const Share = () => {
 
   useEffect(() => {
     if (!recordedStory) return
+
     /**
      * Step 1:
      * Connect to roundware and get a session
      * POST localhost:8888/api/2/sessions/
      * Validate that we got a session throw error is none is given
-     * 
+     *
      * REACT_APP_SESSIONS_URL=https://prod.roundware.com/api/2/sessions/
      */
-    
-    const form = '{"project_id": 1,"client_system": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}'
-    console.log(form)
-    
-    fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_SESSIONS_URL}`, {
-      method: 'POST',
-      headers: {
-        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
-        "content-type": "application/json"
-      },
-      "processData": false,
-      "body": form 
-    })
-    .then((res) => {
-      console.log(res.json())
+    const getSessionId = () => {
+      const nav = window.navigator.userAgent
+      const form = '{"project_id": 1,"client_system": "' + nav.substring(0, 127) + '"}'
+
+      return fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_SESSIONS_URL}`, {
+        method: 'POST',
+        headers: {
+          authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
+          'content-type': 'application/json'
+        },
+        processData: false,
+        body: form
+      }).then(res => res.json())
+        .then((res) => res.id)
+    }
+
+    /**
+    * Step 2:
+    * Create a new envelope
+    * POST localhost:8888/api/2/envelopes/
+    * Save the id returned
+    */
+    const createEnvelope = (sessionId) =>
+      fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_ENVELOPES_URL}`, {
+        method: 'POST',
+        headers: {
+          authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
+          'content-type': 'application/json'
+        },
+        processData: false,
+        body: '{"session_id": ' + sessionId.toString() + '}'
       })
-    .catch(err => console.log(err))
 
-      /**
-      * Step 2:
-      * Create a new envelope 
-      * POST localhost:8888/api/2/envelopes/
-      * Save the id returned
-      */
-      
-      /**
-       * Step 3:
-       * Gather all the info and create a new Formdata class
-       * 
-       */
+    getSessionId()
+      .then(sessionId => createEnvelope(sessionId)
+      )
+      .then((envelopeRes) => {
+        console.log(envelopeRes.json())
+      })
+      .catch(err => console.log(err))
 
-        /**
-        * Step 4:
-        * Update Envelope with Asset
-        * 
-        * PATCH localhost:8888/api/2/envelopes/:id/
-        * 
-        * Validate response
-        */
+    /**
+     * Step 3:
+     * Gather all the info and create a new Formdata class
+     *
+     */
+
+    /**
+    * Step 4:
+    * Update Envelope with Asset
+    *
+    * PATCH localhost:8888/api/2/envelopes/:id/
+    *
+    * Validate response
+    */
   }, [recordedStory])
-    
+
   return (
 
     <div className='SharePage'>
