68c90e3221f5481a8a3a2adb15a5f0625bc14f5c
annarempe <anna.rempe9@gmail.com>
2019-12-16 12:19:26 -0800

    [Anna] add map styling

--- src/components/organisms/MapContainer/MapContainer.jsx
+++ src/components/organisms/MapContainer/MapContainer.jsx
@@ -80,6 +80,290 @@ const MapContainer = (props) => {
           <Map
             google={props.google}
             zoom={11.5}
+            styles={[
+              {
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#f5f5f5'
+                  }
+                ]
+              },
+              {
+                elementType: 'labels.icon',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#616161'
+                  }
+                ]
+              },
+              {
+                elementType: 'labels.text.stroke',
+                stylers: [
+                  {
+                    color: '#f5f5f5'
+                  }
+                ]
+              },
+              {
+                featureType: 'administrative',
+                stylers: [
+                  {
+                    visibility: 'simplified'
+                  }
+                ]
+              },
+              {
+                featureType: 'administrative',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#bdbdbd'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#eeeeee'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi',
+                elementType: 'labels.text',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#757575'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#e5e5e5'
+                  }
+                ]
+              },
+              {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#9e9e9e'
+                  }
+                ]
+              },
+              {
+                featureType: 'road',
+                stylers: [
+                  {
+                    weight: 0.5
+                  }
+                ]
+              },
+              {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    visibility: 'simplified'
+                  },
+                  {
+                    weight: 1
+                  }
+                ]
+              },
+              {
+                featureType: 'road',
+                elementType: 'geometry.fill',
+                stylers: [
+                  {
+                    color: '#f2cc90'
+                  }
+                ]
+              },
+              {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [
+                  {
+                    color: '#55ef00'
+                  }
+                ]
+              },
+              {
+                featureType: 'road',
+                elementType: 'labels.icon',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.arterial',
+                elementType: 'labels',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.arterial',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#757575'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#dadada'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.highway',
+                elementType: 'labels',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#616161'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.local',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.local',
+                elementType: 'labels',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#9e9e9e'
+                  }
+                ]
+              },
+              {
+                featureType: 'transit',
+                stylers: [
+                  {
+                    visibility: 'off'
+                  }
+                ]
+              },
+              {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#e5e5e5'
+                  }
+                ]
+              },
+              {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#eeeeee'
+                  }
+                ]
+              },
+              {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [
+                  {
+                    color: '#b5c6d5'
+                  }
+                ]
+              },
+              {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [
+                  {
+                    color: '#9a999a'
+                  }
+                ]
+              }
+            ]}
             className='Map'
             initialCenter={{ lat: 42.3601, lng: -71.05 }}
           >{displayMarkers()}
