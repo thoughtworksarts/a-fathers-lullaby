a610a4547c25bf8b9186db268ea88028cb036e0d
annarempe <anna.rempe9@gmail.com>
2019-12-20 15:49:49 -0700

    [Anna] add logo for mobile

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import menuBars from 'assets/menu-bars.svg'
 import { Logo, Link, Subtitle, NavLink, Backdrop } from 'atoms'
 import { Sidepanel } from 'organisms'
+import mobileLogo from 'assets/mobileLogo.png'
+
 import './Header.css'
 
 const Header = () => {
@@ -21,33 +23,48 @@ const Header = () => {
       setIsOpen(!isOpen)
     }
     const menu = document.getElementsByClassName('sideMenu')[0]
-    if (menu) { menu.addEventListener('click', backdropClickHandler) }
+    if (menu) {
+      menu.addEventListener('click', backdropClickHandler)
+    }
   })
 
   return (
     <nav className='Header'>
-      <div className='logoText'>
-        <Link to='/'><Logo size='sm' /></Link>
-        <Subtitle />
-      </div>
-      {
-        isMobile
-          ? (
-            <div className='sideMenu'>
-              <img className='menu-bars' src={menuBars} alt='Menu' onClick={() => setIsOpen(true)} />
-              <Sidepanel show={isOpen} />
-              <Backdrop alt='Backdrop' show={isOpen} />
-            </div>
-
-          )
-          : (
-            <div className='NavBar NavLinks'>
-              <NavLink className='headerLinks' link='/'>About</NavLink>
-              <NavLink link='/share'>Share</NavLink>
-              <NavLink link='/explore'>Explore</NavLink>
-            </div>
-          )
-      }
+      {isMobile ? (
+        <div className='mobile-header'>
+          <div className='mobile-logo'>
+            <Link to='/'>
+              <img src={mobileLogo} alt='mobile AFL logo' />
+            </Link>
+          </div>
+          <div className='sideMenu'>
+            <img
+              className='menu-bars'
+              src={menuBars}
+              alt='Menu'
+              onClick={() => setIsOpen(true)}
+            />
+            <Sidepanel show={isOpen} />
+            <Backdrop alt='Backdrop' show={isOpen} />
+          </div>
+        </div>
+      ) : (
+        <div className='desktop-header'>
+          <div className='logoText'>
+            <Link to='/'>
+              <Logo size='sm' />
+            </Link>
+            <Subtitle />
+          </div>
+          <div className='NavBar NavLinks'>
+            <NavLink className='headerLinks' link='/'>
+              About
+            </NavLink>
+            <NavLink link='/share'>Share</NavLink>
+            <NavLink link='/explore'>Explore</NavLink>
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
