280dda6a4263121b2eb4894410b1f5e3c270e227
annarempe <anna.rempe9@gmail.com>
2019-12-23 13:13:49 -0700

    [#123][Anna] move second par down for intro in mobile

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -5,6 +5,8 @@ import './About.css'
 
 const About = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 980)
+  const [firstParClass, setFirstParClass] = useState('')
+  const [secondParClass, setSecondParClass] = useState('')
 
   useEffect(() => {
     const handleResize = () => {
@@ -13,26 +15,27 @@ const About = () => {
     window.addEventListener('resize', handleResize)
   }, [])
 
-  const createMobileSecondParagraph = () => {
+  useEffect(() => {
+    isMobile
+      ? setFirstParClass('mobile-intro-par1')
+      : setFirstParClass('desktop-intro-par1')
+  }, [isMobile])
 
-  }
+  useEffect(() => {
+    isMobile
+      ? setSecondParClass('mobile-intro-par2')
+      : setSecondParClass('desktop-intro-par2')
+  }, [isMobile])
 
-  const createDesktopSecondParagraph = () => {
-    return (
-      <h2 className='desktop-intro-par2'>Personal stories have transformative power and create profound encounters. A Father's Lullaby invites you to become part of a growing movement.</h2>
-    )
-  }
-
+  const secondPar = <h2 className={secondParClass}>Personal stories have transformative power and create profound encounters. A Father's Lullaby invites you to become part of a growing movement.</h2>
   return (
     <div className='AboutPage'>
       <div className='intro'>
-        <h1 className='desktop-intro-par1'>There are <span className='highlight'>2.7 million children</span> with a parent in prison or jail. 92% of parents <span className='highlight'>in prison are fathers.</span></h1>
+        <h1 id='first-par' className={firstParClass}>There are <span className='highlight'>2.7 million children</span> with a parent in prison or jail. 92% of parents <span className='highlight'>in prison are fathers.</span></h1>
         <Button to='/share'>Participate</Button>
-        {isMobile
-          ? createMobileSecondParagraph()
-          : createDesktopSecondParagraph()}
-
+        {isMobile ? <div /> : secondPar}
       </div>
+      {isMobile ? secondPar : <div />}
       <Timeline />
     </div>
   )
