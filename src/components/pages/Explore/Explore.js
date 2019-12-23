c067319f99665920b6d5997239803fd0257c4c5e
annarempe <anna.rempe9@gmail.com>
2019-12-23 13:30:47 -0700

    [Anna] hide playStream button

--- src/components/pages/Explore/Explore.js
+++ src/components/pages/Explore/Explore.js
@@ -5,6 +5,7 @@ import { Row, Col, Container } from 'react-bootstrap'
 import './Explore.css'
 
 const Explore = () => {
+  const displayPlayStream = false
   const [stories, setStories] = useState([])
   const [tags, setTags] = useState([])
   const [currentStory, setCurrentStory] = useState('')
@@ -15,11 +16,14 @@ const Explore = () => {
   const { id } = useParams()
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_ASSETS_URL}`, {
-      headers: {
-        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
+    fetch(
+      `${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_ASSETS_URL}`,
+      {
+        headers: {
+          authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
+        }
       }
-    })
+    )
       .then(res => res.json())
       .then(stories => {
         setStories(stories)
@@ -47,11 +51,14 @@ const Explore = () => {
   }
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_TAGS_URL}`, {
-      headers: {
-        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
+    fetch(
+      `${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_TAGS_URL}`,
+      {
+        headers: {
+          authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`
+        }
       }
-    })
+    )
       .then(res => res.json())
       .then(tags => {
         setTags(tags)
@@ -61,24 +68,29 @@ const Explore = () => {
 
   const getSessionId = () => {
     const nav = window.navigator.userAgent
-    const form = '{"project_id": 25,"client_system": "' + nav.substring(0, 127) + '"}'
+    const form =
+      '{"project_id": 25,"client_system": "' + nav.substring(0, 127) + '"}'
 
-    return fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_SESSIONS_URL}`, {
-      method: 'POST',
-      headers: {
-        authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
-        'content-type': 'application/json'
-      },
-      processData: false,
-      body: form
-    }).then(res => res.json())
-      .then((res) => {
+    return fetch(
+      `${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_SESSIONS_URL}`,
+      {
+        method: 'POST',
+        headers: {
+          authorization: `token ${process.env.REACT_APP_ROUNDWARE_TOKEN}`,
+          'content-type': 'application/json'
+        },
+        processData: false,
+        body: form
+      }
+    )
+      .then(res => res.json())
+      .then(res => {
         const sessionId = res.id
         return sessionId
       })
   }
 
-  const createFormData = (sessionId) => {
+  const createFormData = sessionId => {
     const formData = new FormData()
     formData.append('session_id', sessionId.toString())
     formData.append('latitude', latitude)
@@ -87,8 +99,9 @@ const Explore = () => {
     return formData
   }
 
-  const createStream = (formData) => {
-    return fetch(`${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_STREAMS_URL}`,
+  const createStream = formData => {
+    return fetch(
+      `${process.env.REACT_APP_CORS_ANYWHERE}/${process.env.REACT_APP_STREAMS_URL}`,
       {
         method: 'POST',
         headers: {
@@ -98,36 +111,55 @@ const Explore = () => {
         processData: false,
         contentType: false,
         body: formData
-      })
+      }
+    )
       .then(res => {
         return res.json()
       })
       .catch(err => console.log(err))
   }
 
-  const updateCurrentStory = (newCurrentStory) => {
+  const updateCurrentStory = newCurrentStory => {
     setCurrentStory(newCurrentStory)
   }
 
   return (
     <Container className='explore-page'>
+      {displayPlayStream ? (
+        <Row>
+          <Col lg={12}>
+            <button className='Button' onClick={playStream}>
+              {isPlaying ? 'Pause Stream' : 'Play Stream'}
+              <audio key={mp3URL} id='streamplayer'>
+                <source id='audiosource' type='audio/mp3' src={mp3URL} />
+              </audio>
+            </button>
+          </Col>
+        </Row>
+      ) : (
+        <div />
+      )}
+
       <Row>
         <Col lg={12}>
-          <button className='Button' onClick={playStream}>{isPlaying ? 'Pause Stream' : 'Play Stream'}
-            <audio key={mp3URL} id='streamplayer'>
-              <source id='audiosource' type='audio/mp3' src={mp3URL} />
-            </audio>
-          </button>
+          <MapContainer
+            stories={stories}
+            id={id}
+            currentStory={currentStory}
+            parentCallback={updateCurrentStory}
+            tags={tags}
+          />
         </Col>
       </Row>
       <Row>
         <Col lg={12}>
-          <MapContainer stories={stories} id={id} currentStory={currentStory} parentCallback={updateCurrentStory} tags={tags} />
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={12}>
-          <StoryPlaylist className='ExplorePlaylist' stories={stories} id={id} currentStory={currentStory} updateCurrentStory={updateCurrentStory} />
+          <StoryPlaylist
+            className='ExplorePlaylist'
+            stories={stories}
+            id={id}
+            currentStory={currentStory}
+            updateCurrentStory={updateCurrentStory}
+          />
         </Col>
       </Row>
     </Container>
