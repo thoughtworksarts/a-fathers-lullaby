b378e50168b5a25a7abba38f4c3a339045a8d070
Tim Brandle <tbrandle53@gmail.com>
2019-12-30 15:29:44 -0700

    [Tim] add StoryPlayer component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,6 +2,7 @@ import Footer from './Footer'
 import TimelineText from './TimelineText'
 import TimelinePicture from './TimelinePicture'
 import Story from './Story'
+import StoryPlayer from './StoryPlayer'
 import StoryView from './StoryView'
 import Intro from './Intro'
 
@@ -10,6 +11,7 @@ export {
   TimelineText,
   TimelinePicture,
   Story,
+  StoryPlayer,
   StoryView,
   Intro
 }
